875bad4445815f9c0a5d92134070a58ca0d82e73
Betsy Sanchez <bedesanchezca@ittepic.edu.mx>
2019-10-28 11:54:35 -0700

    done

--- src/components/listas/listas.ts
+++ src/components/listas/listas.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { DeseosProvider } from '../../providers/deseos/deseos';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AgregarPage } from '../../pages/agregar/agregar';
 import { Lista } from '../../app/models/lista.model';
 
@@ -18,7 +18,8 @@ export class ListasComponent {
 
   text: string;
   @Input() completada: boolean = true;
-  constructor(private deseosService: DeseosProvider, private navCtrl: NavController) {
+  constructor(private deseosService: DeseosProvider, private navCtrl: NavController
+  , private alertCtrl: AlertController) {
   }
 
   listaSeleccionada(lista: Lista){
@@ -26,5 +27,42 @@ export class ListasComponent {
   }
   borrarLista(lista: Lista){
     this.deseosService.borrarLista(lista);
+  }
+
+  async editarLista(lista: Lista){
+      const alert = await this.alertCtrl.create({
+        title: 'Editar lista',
+        inputs: [
+          {
+            name: 'titulo',
+            type: 'text',
+            value: lista.titulo,
+            placeholder: 'Nombre de la lista'
+          },
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: () => {
+              console.log('Confirm Cancel');
+            }
+          }, {
+            text: 'Actualizar',
+            handler: (data) => {
+              console.log(data);
+              if (data.titulo.length === 0){
+                return;
+              }
+             lista.titulo = data.titulo;
+             this.deseosService.guardarStorage();
+            }
+          }
+        ]
+      });
+      await alert.present();
+      //this.navCtrl.push(AgregarPage);
+
   }
 }
